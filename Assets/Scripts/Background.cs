cb070aca3cd745a1676995e5e31ffada91273c13
Nickso Calheiros <nicksofacanha@gmail.com>
2021-07-21 18:46:38 -0400

    Update para o Trabalho final
    adicionado:
    Mecânica de Jogo com pontuação
    UI com quatro telas
    Música e SFX
    Usar recursos diferentes

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Vector2 saved0ffset;
+    private Renderer renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TryGetComponent (out renderer);
+        saved0ffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Mathf.Repeat (Time.time * speed, 1);
+        Vector2 offset = new Vector2(saved0ffset.x, y);
+        renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);    
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject hud;
+    [SerializeField] private GameObject pause;
+    [SerializeField] private GameObject gameOver;
+    [SerializeField] private GameObject ship;
+    [SerializeField] private GameObject enemies;
+    [SerializeField] private Button pauseBtn;
+    [SerializeField] private Sprite playSprite, pauseSprite;
+    [SerializeField] private Text scoreTxt;
+    [SerializeField] private Text livesTxt;
+    [SerializeField] private AudioSource music, pauseSFX;
+    private int points;
+    private bool started;
+
+    public static GameController Instance;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreTxt.text = "0";
+        mainMenu.SetActive(true);
+        hud.SetActive(false);
+        pause.SetActive(false);
+        gameOver.SetActive(false);
+
+        pauseBtn.onClick.AddListener(Pause);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            if (!started) StartGame();
+            else if (gameOver.activeSelf) SceneManager.LoadScene(0);
+        }
+    }
+
+    void StartGame () 
+    {
+        started = true;
+        mainMenu.SetActive(false);
+        hud.SetActive(true);
+        enemies.SetActive(true);
+        ship.SetActive(true);
+    }
+
+    public void UpdateScore (int _points) 
+    {
+        points += _points;
+        scoreTxt.text= points.ToString();
+    }
+
+    public void UpdateLives (int lives)
+    {
+        livesTxt.text = "x" + lives;
+    }
+
+    public void Pause ()
+    {
+        bool paused = pause.activeSelf;
+        paused = !paused;
+        pause.SetActive(paused);
+
+        pauseSFX.Play();
+        music.volume = paused ? 0.01f : 0.09f;
+
+        Time.timeScale = paused ? 0 : 1;
+        pauseBtn.image.sprite = paused? playSprite: pauseSprite;
+    }
+
+    public void GameOver ()
+    {
+        hud.SetActive(false);
+        gameOver.SetActive(true);
+    }
+}
